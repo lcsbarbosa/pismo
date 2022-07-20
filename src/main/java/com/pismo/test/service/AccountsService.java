918e09b9b69691faec1d725aa8ee1e5bcdcf840b
w504649 <lucas.alves@wexinc.com>
2022-07-20 16:39:27 -0300

    creating Service Layer

--- src/main/java/com/pismo/test/service/AccountsService.java
+++ src/main/java/com/pismo/test/service/AccountsService.java
@@ -1,9 +1,12 @@
 package com.pismo.test.service;
 
+import com.pismo.test.exceptions.AccountsNotFoundException;
 import com.pismo.test.model.Accounts;
 import com.pismo.test.repository.AccountsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AccountsService {
@@ -14,4 +17,16 @@ public class AccountsService {
     public Accounts create(Accounts accounts){
         return this.repository.save(accounts);
     }
+
+    public List<Accounts> getAccounts() {
+        return repository.findAll();
+    }
+
+    public Accounts getAccountById(Long id) {
+        Accounts account = this.repository.findById(id)
+                            .orElseThrow(() -> new AccountsNotFoundException(id));
+        return account;
+    }
+
+
 }
--- src/main/java/com/pismo/test/service/TransactionsService.java
+++ src/main/java/com/pismo/test/service/TransactionsService.java
@@ -1,9 +1,17 @@
 package com.pismo.test.service;
 
+import com.pismo.test.exceptions.TransactionNotFoundException;
 import com.pismo.test.model.Transactions;
+import com.pismo.test.model.dto.TransactionsDTO;
 import com.pismo.test.repository.TransactionsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @Service
 public class TransactionsService {
@@ -11,7 +19,21 @@ public class TransactionsService {
     @Autowired
     TransactionsRepository repository;
 
-    public Transactions create(Transactions transactions){
-        return repository.save(transactions);
+    public List<Transactions> getTransactions() {
+        return repository.findAll();
     }
+
+    public Transactions getTransactionById(Long id) {
+        Transactions transactions = this.repository.findById(id)
+                                    .orElseThrow(() -> new TransactionNotFoundException(id));
+        return transactions;
+    }
+
+    public ResponseEntity<TransactionsDTO> create(Transactions transactions){
+        Transactions transaction = new Transactions(transactions);
+        repository.save(transaction);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(new TransactionsDTO(transactions));
+    }
+
 }
