b655008526fdccb243aa0353f3108c44f0dcb4a0
w504649 <lucas.alves@wexinc.com>
2022-07-20 16:41:53 -0300

    Adding Compoent Scan

--- src/main/java/com/pismo/test/TestApplication.java
+++ src/main/java/com/pismo/test/TestApplication.java
@@ -2,10 +2,11 @@ package com.pismo.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(basePackages = "com.pismo.test.repository")
+@ComponentScan(basePackages = {"com.pismo.test.service", "com.pismo.test.controller"})
 @SpringBootApplication
 public class TestApplication {
 
